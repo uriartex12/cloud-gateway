536191267f332160a0a66f6737e070014abcfc47
uriartex12 <prietomatex_1998@hotmail.com>
2024-12-29 15:09:16 -0500

    frist commit

---
+++ src/main/java/com/sgi/cloud_gateway/SgiCloudGatewayApplication.java
@@ -0,0 +1,12 @@
+package com.sgi.cloud_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgiCloudGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgiCloudGatewayApplication.class, args);
+	}
+}
